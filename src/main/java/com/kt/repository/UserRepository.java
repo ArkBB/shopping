6732f0602e32da564ee121b0b372b5ad23554fcb
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-11-03 15:47:32 +0900

    feat: user repository 회원가입 개발

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -16,9 +16,36 @@ public class UserRepository {
 
 	public void save(User user) {
 		// 서비스에서 dto를 도메인(비지니스모델)으로 바꾼다음 전달
-		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
 
-		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
+		var sql = """
+				INSERT INTO MEMBER (
+									id,
+									loginId,
+									password,
+									name,
+									birthday,
+									mobile,
+									email,
+									gender,
+									createdAt,
+									updatedAt
+									) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+				
+				""";
+
+		jdbcTemplate.update(
+				sql,
+				user.getId(),
+				user.getLoginId(),
+				user.getPassword(),
+				user.getName(),
+				user.getBirthday(),
+				user.getMobile(),
+				user.getEmail(),
+				user.getGender(),
+				user.getCreatedAt(),
+				user.getUpdatedAt()
+		);
 	}
 
 	public Long selectMaxId() {
